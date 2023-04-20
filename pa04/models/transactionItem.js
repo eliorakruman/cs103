c759c850276d2ee98a98207bbd71f15c997a54c0
Eliora Kruman <krumaneliora@gmail.com>
2023-04-19 23:50:09 -0400

    added model, view, and route for transaction app. added to app.js and layout

---
+++ pa04/models/transactionItem.js
@@ -0,0 +1,14 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var transactionItemSchema = Schema( {
+  item: String,
+  amount: Number,
+  category: String,
+  occurredAt: Date,
+  userId: ObjectId
+} );
+
+module.exports = mongoose.model( 'transactionItem', transactionItemSchema );
---
+++ pa04/routes/transaction.js
@@ -0,0 +1,69 @@
+/*
+  transaction.js -- Router for the Transaction
+*/
+const express = require('express');
+const router = express.Router();
+const transactionItem = require('../models/transactionItem')
+
+
+/*
+this is a very simple server which maintains a key/value
+store using an object where the keys and values are lists of strings
+
+*/
+
+isLoggedIn = (req,res,next) => {
+  if (res.locals.loggedIn) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+// get the value associated to the key
+router.get('/transaction/',
+  isLoggedIn,
+  async (req, res, next) => {
+      const completed = req.query.show=='completed'
+      res.locals.show = req.query.show
+      res.locals.items = 
+        await transactionItem.find(
+           {userId:req.user._id, createdAt}).sort({createdAt:1})
+      res.render('transactions');
+});
+
+router.post('/transaction',
+  isLoggedIn,
+  async (req, res, next) => {
+      const todo = new transactionItem(
+        {item:req.body.item,
+          amount: parseInt(req.body.amount),
+          category: req.body.category,
+          occurredAt: req.body.date,
+         userId: req.user._id
+        })
+      await todo.save();
+      res.redirect('/transaction')
+});
+
+router.get('/transaction/remove/:itemId',
+  isLoggedIn,
+  async (req, res, next) => {
+      console.log("inside /transaction/remove/:itemId")
+      await transactionItem.deleteOne({_id:req.params.itemId});
+      res.redirect('/transaction')
+});
+
+//create a transaction edit view
+router.get('/transaction/edit/:itemId',
+  isLoggedIn,
+  async (req, res, next) => {
+      console.log("inside /transaction/edit/:itemId")
+      await transactionItem.findOneAndUpdate(
+        {_id:req.params.itemId},
+        {$set: {item:req.body.item,
+        amount: parseInt(req.body.amount),
+      category: req.body.category,
+    occuredAt: req.body.date}} );
+      res.redirect('/transaction')
+});
